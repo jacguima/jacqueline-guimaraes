488aab0d95ae46db8195cd2ea50b42e08157bcd6
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-10-12 22:54:40 -0300

    Replace profile image and simplify AboutMeMobile component
    Updated the AboutMeMobile component to use a new background image instead of the previous profile picture. Removed unnecessary title container and associated styles, simplifying the component structure and improving maintainability.

--- src/componentes/about-me-mobile/about-me-mobile.js
+++ src/componentes/about-me-mobile/about-me-mobile.js
@@ -1,40 +1,13 @@
 import "./about-me-mobile.css";
-import ProfilePicture from "../../assets/images/foto2jac.png";
+import ProfileBG from "../../assets/images/about-me-profile.png";
 
 const AboutMeMobile = () => {
   return (
-    <div className="about-me-mobile container-narrow">
-      <div className={"title-container"}>
-        <svg viewBox="0 0 600 300" xmlns="http://www.w3.org/2000/svg">
-          <path
-            id="curve"
-            fill="transparent"
-            d="M 50,200 A 400,400 0 0,1 550,200"
-          />
-
-          <text width="500" font-size="30" fill="#7d7d4a">
-            <textPath href="#curve" startOffset="50%" text-anchor="middle">
-              Sobre mim
-            </textPath>
-          </text>
-        </svg>
-      </div>
-
-      <div className="profile-picture-container">
-        <img
-          className="profile-picture"
-          src={ProfilePicture}
-          alt="Profile Pictue"
-        />
-      </div>
-
-      <div className={"text-content"}>
-        <h2 className={"content-title"}>
-          Olá, Sou a<br />
-          Jacqueline Guimarães
-        </h2>
-        <p className={"profession"}>PSICÓLOGA CLÍNICA</p>
-        <p className={"dots"}>&#9675; &#9675; &#9675; &#9675;</p>
+    <div className="container-narrow">
+      <div className="about-me-mobile">
+        <div className={"profile-bg-container"}>
+          <img className={"profile-bg"} src={ProfileBG} alt="Profile BG" />
+        </div>
         <div className={"paragraphs-container"}>
           <p>
             Graduada pelo UniCEUB e inscrita no Conselho Regional de Psicologia
