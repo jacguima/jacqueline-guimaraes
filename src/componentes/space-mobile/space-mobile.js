26354291e0c27b5c1860fced355e81b61f00cf75
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-10-19 16:58:29 -0300

    Add address cards and update background for space-mobile
    Added detailed address cards for Asa Norte and Asa Sul locations with map images, buttons, and styled elements. Updated the background image of the mobile space section to enhance the design.

--- src/componentes/space-mobile/space-mobile.js
+++ src/componentes/space-mobile/space-mobile.js
@@ -1,9 +1,6 @@
 import "./space-mobile.css";
 import MapSouth from "../../assets/images/south.png";
 import MapNorth from "../../assets/images/north.png";
-import Pin from "../../assets/images/pin.png";
-import Phone from "../../assets/images/celular.png";
-import CheckIcon from "../../shared/check-icon";
 
 const SpaceMobile = () => {
   return (
@@ -13,6 +10,30 @@ const SpaceMobile = () => {
         style={{ display: "flex", flexDirection: "column" }}
       >
         <h2 className={"title"}>O espaço</h2>
+        <div className={"cards-container"}>
+          <div className={"card"}>
+            <div className={"card-map-img-wrapper"}>
+              <img className={"card-map-img"} src={MapNorth} alt="Mapa" />
+            </div>
+            <div className={"address"}>
+              <h2 className={"address-title"}>Asa Norte</h2>
+              <h3 className={"address-details"}>Shopping ID</h3>
+            </div>
+            <button className={"card-button"}>Como chegar</button>
+          </div>
+          <div className={"card"}>
+            <div className={"card-map-img-wrapper"}>
+              <img className={"card-map-img"} src={MapSouth} alt="Mapa" />
+            </div>
+            <div className={"address"}>
+              <h2 className={"address-title"}>Asa Sul</h2>
+              <h3 className={"address-details"}>
+                Ed. San Marino - SEP Sul <br /> Quadra 707/907
+              </h3>
+            </div>
+            <button className={"card-button"}>Como chegar</button>
+          </div>
+        </div>
       </div>
     </div>
   );
