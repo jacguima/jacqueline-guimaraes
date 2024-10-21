d298c773f14a5e6586a73d9954ee382cb90f3af6
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-10-21 00:58:38 -0300

    Add overlay and remove circle background in reviews
    Added a gradient overlay to `.carousel-container` for improved visual presentation. Removed the `.circle-bg-reviews` element and its associated styles from the reviews component for a cleaner layout.

--- src/componentes/reviews/reviews.js
+++ src/componentes/reviews/reviews.js
@@ -6,7 +6,6 @@ const Reviews = () => {
   return (
     <div className="reviews-container">
       <div className="container-narrow" style={{ padding: 0 }}>
-        <div className="circle-bg-reviews"></div>
         <Carousel />
       </div>
     </div>
