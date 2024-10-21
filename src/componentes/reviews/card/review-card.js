1367c0fa9b374440cf291ef93fd2f6de29c52638
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-10-20 23:28:13 -0300

    Refactor ReviewCard component to use dynamic link prop
    Replaced the static `source` prop with a dynamic `link` prop to streamline source handling. Updated relevant components and mock data to support this change, ensuring better flexibility for future integrations.

--- src/componentes/reviews/card/review-card.js
+++ src/componentes/reviews/card/review-card.js
@@ -4,45 +4,20 @@ import DoctaraliaIcon from "../../../assets/images/doctoria.png";
 import GoogleIcon from "../../UI/google";
 import BgReviewImage from "../../../assets/images/review-bg.png";
 
-const ReviewCard = ({ author, content, source }) => {
+const ReviewCard = ({ author, content, link }) => {
   const renderSourceIcon = () => {
-    switch (source) {
-      case "google":
-        return <GoogleIcon />;
-      case "doctoralia":
-        return (
-          <img
-            src={DoctaraliaIcon}
-            alt="doctoralia"
-            className="doctoralia-icon"
-          />
-        );
-
-      default:
-        return null;
-    }
-  };
-
-  let google =
-    "https://www.google.com/search?q=psic%C3%B3loga+jacqueline+guimar%C3%A3es";
-  let doctoralia =
-    "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia";
-  const renderSourceLink = () => {
-    switch (source) {
-      case "google":
-        return google;
-      case "doctoralia":
-        return doctoralia;
-      default:
-        return null;
-    }
+    return link?.includes("doctoralia") ? (
+      <img src={DoctaraliaIcon} alt="doctoralia" className="doctoralia-icon" />
+    ) : (
+      <GoogleIcon />
+    );
   };
 
   return (
     <div className="card-review-container">
       <div className="card-review">
         <div className="source-icon">
-          <a target="_blank" rel="noreferrer" href={renderSourceLink()}>
+          <a target="_blank" rel="noreferrer" href={link}>
             {renderSourceIcon()}
           </a>
         </div>
--- src/componentes/reviews/reviews-mock.js
+++ src/componentes/reviews/reviews-mock.js
@@ -1,81 +1,119 @@
 const reviews = [
   {
-    id: 1,
-    author: "A.C",
+    author: "Alice",
     content:
       "Faz já mais de um mês que tenho consulta com a Jacqueline e simplesmente amei ela desde o primeiro dia! Um amor de pessoa, muito atenciosa e sempre me ajuda. Obrigada Jacqueline!",
-    rating: 5,
-    source: "doctoralia",
+    link: "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia",
   },
   {
-    id: 2,
-    author: "G.S",
+    author: "Gabriela",
     content:
       "Sentimento de acolhimento, ótima troca de ideias e sem julgamentos. Tratamento excelente. 🤍🤍",
-    rating: 5,
-    source: "google",
+    link: "https://g.co/kgs/oPJWv7D",
   },
   {
-    id: 3,
-    author: "A.O",
+    author: "Agda",
     content:
       "A doutora Jacqueline Guimarães é uma excelente profissional, tem escuta ativa, me auxiliou em uma demanda muito complicada, sempre com respeito ao meu sentimento e empatia.",
-    rating: 5,
-    source: "doctoralia",
+    link: "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia",
   },
   {
-    id: 4,
-    author: "L.A",
+    author: "Leci",
     content:
       "Adoro ela! Sofria de uma ansiedade forte mas depois que comecei o tratamento com ela processo muito melhor minhas emoções. Obrigada minha psicóloga.",
-    rating: 5,
-    source: "google",
+    link: "https://g.co/kgs/Mtsp24u",
   },
   {
-    id: 5,
-    author: "A.K",
+    author: "Ana",
     content:
       "Estou fazendo terapia de casal com o meu marido, e nossa primeira consulta foi excelente! A Dra Jacqueline nos deixou bem livre para expor nossas inseguranças e no final dessa primeira sessão já nos passou um exercício, tenho certeza que esse acompanhamento terapêutico trará bons frutos ao meu casamento, gratidão doutra!",
-    rating: 5,
-    source: "doctoralia",
+    link: "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia",
   },
   {
-    id: 6,
-    author: "A.A",
+    author: "Aline",
     content: "Profissional excelente!!Simpática e atenciosa. Recomendo muito!!",
-    rating: 5,
-    source: "google",
+    link: "https://g.co/kgs/MTYPbgs",
   },
   {
-    id: 7,
-    author: "R.P",
+    author: "Rayanne",
     content: "Excelente profissional, atenciosa e muito fofa!!!!",
-    rating: 5,
-    source: "doctoralia",
+    link: "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia",
   },
   {
-    id: 8,
-    author: "V.H",
+    author: "Victor",
     content:
       "Excelente profissional! Mais de 1 ano de muito comprometimento, me deixou à vontade desde a primeira sessão, me ajuda em diversas coisas e sinto que cresci em todas as áreas da minha vida nesse tempo. Recomendo demais!",
-    rating: 5,
-    source: "google",
+    link: "https://g.co/kgs/efw31F5",
   },
   {
-    id: 9,
-    author: "A.L",
+    author: "Arthur",
     content:
       "Primeiro atendimento bastante satisfatório. Estou confiante para dar continuidade ao tratamento.",
-    rating: 5,
-    source: "doctoralia",
+    link: "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia",
   },
   {
-    id: 10,
-    author: "G.S",
+    author: "Yasmin",
     content:
-      "A Jacque é atenciosa, realista e jeitosa. Há 2 anos ela me atende, e eu já fui do 8 a 80, e ela sempre me atendeu bem, com carinho, compreensão, cuidado e gentileza, me fazendo amadurecer e crescer bastante. Como uma pessoa que já passou por muitos profissionais da área da psicologia, fico feliz em dizer que ela é uma das melhores que já me atenderam.",
-    rating: 5,
-    source: "google",
+      "A Jacque é atenciosa, realista e jeitosa. Há 2 anos ela me atende, e eu já fui do 8 a 80, e ela sempre me atendeu bem, com carinho, compreensão, cuidado e gentileza, me fazendo amadurecer e crescer bastante. Como uma pessoa que já passou por muitos profissionais da área da psicologia, fico feliz em dizer que ela é uma das melhores psicólogas que já me atenderam.",
+    link: "https://g.co/kgs/nonJ2aw",
+  },
+  {
+    author: "Wellington",
+    content:
+      "Muito atenciosa, comunicativa! Se preocupa em deixar o ambiente confortável, te trás confiança, preocupada em te dar conforto, simpática!",
+    link: "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia",
+  },
+  {
+    author: "Maria",
+    content:
+      "Dra. Jacqueline é uma excelente profissional! Sempre muito simpática, atenciosa e empática! Recomendo o seu atendimento de olhos fechados",
+    link: "https://g.co/kgs/DxJihKs",
+  },
+  {
+    author: "A.A.",
+    content: "Ótima consulta! Me senti muito confortável em conversar com ela.",
+    link: "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia",
+  },
+  {
+    author: "Brenda",
+    content:
+      "…sempre era muito fluido conversar sobre o que estivesse me passando pela cabeça, a Jacqueline sempre se mostrou muito prestativa e uma ótima ouvinte, sinto que cresci muito durante esse meio tempo que fiz esse acompanhamento e agradeço imensamente por todo esse período que dedicamos ao autoconhecimento e a lidar com minhas emoções e a das outras pessoas ao meu redor!! Muito grata mesmo.",
+    link: "https://g.co/kgs/kbuvgLC",
+  },
+  {
+    author: "William",
+    content:
+      "Gostei bastante do atendimento da Jacque. Demonstra bastante atenção e passa um sentimento de cuidado e acolhimento necessário para um psicólogo. Gostei!",
+    link: "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia",
+  },
+  {
+    author: "Bruna",
+    content: "Excelente profissional. Muito resiliente, humana e acolhedora.",
+    link: "https://g.co/kgs/tDjXfc9",
+  },
+  {
+    author: "João",
+    content:
+      "Sou muito grato por ter conhecido a dr.a Jacqueline! Ela me ajudou a evoluir muito como ser humano, e me ensinou bastante sobre trabalhar a minha individualidade e controlar minhas emoções. Sem dúvidas, a melhor psicóloga que já conheci!!!! Parabéns pela excelência e competência.",
+    link: "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia",
+  },
+  {
+    author: "Rayanne",
+    content:
+      "Ela é psicóloga da minha filha de 6 anos, tem uma didática incrível para lidar com ela, além de toda a sensibilidade em torno da história e acontecimentos! Conduz muito bem as análises para que minha filha tenha a percepção necessária e de acordo com a idade. Ela foi a melhor escolha que pude fazer nesse processo para a minha filha!",
+    link: "https://g.co/kgs/oZ8mFfQ",
+  },
+  {
+    author: "Kedma",
+    content:
+      "Consultas excelentes! Jaqueline é extremamente educada e competente. As conversas com ela estão sendo bem proveitosas e necessárias pra mim. Estou gostando muito!",
+    link: "https://www.doctoralia.com.br/jacqueline-guimaraes-3/psicologo/brasilia",
+  },
+  {
+    author: "Alícia",
+    content:
+      "A dra. Jacqueline me ajudou num momento em que eu achei que não tinha mais sentido estar aqui. Me motiva a cada sessão a ser uma pessoa melhor, com as minhas próprias decisões mas vistas sempre de um ângulo diferente. Sempre que saio da sessão sinto que sou capaz de fazer qualquer coisa. Me auxiliou com diagnósticos que eu só pensava que poderiam existir e me fez entender que eu sou assim mesmo, independente do que os outros achem. Indico a dra. Jacqueline para todos e qualquer pessoa, pois ela é muito especial!",
+    link: "https://g.co/kgs/cb7oPxX",
   },
 ];
 
