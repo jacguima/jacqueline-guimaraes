6f32c3b27108e11c2c5490afa5a83ad98d484f08
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2024-11-19 15:31:11 -0300

    Improve map and address hover interactions
    Added hover effects to map and address elements in SpaceDesktop using useEffect and useRef hooks. Enhanced the user experience by scaling elements on hover and implementing a smoother transition, making the interface more interactive and visually appealing.

--- src/componentes/space/space-desktop/space-desktop.js
+++ src/componentes/space/space-desktop/space-desktop.js
@@ -5,8 +5,64 @@ import Pin from "../../../assets/images/pin.png";
 import Phone from "../../../assets/images/phone.png";
 import CheckIcon from "../../../shared/check-icon";
 import ScrollingHighlightText from "../../../shared/scrolling-highlight-text/scrolling-highlight-text";
+import { useEffect, useRef } from "react";
+import { openLink } from "../../../shared/utils";
 
 const SpaceDesktop = () => {
+  const northMapRef = useRef(null);
+  const northAddressRef = useRef(null);
+  const southMapRef = useRef(null);
+  const southAddressRef = useRef(null);
+
+  useEffect(() => {
+    const handleHover = (mapElement, addressElement) => {
+      let isHoveringMap = false;
+      let isHoveringAddress = false;
+
+      const scaleUp = () => {
+        mapElement.classList.add("hovered");
+        addressElement.classList.add("hovered", "delay-scale");
+      };
+
+      const scaleDown = () => {
+        mapElement.classList.remove("hovered");
+        addressElement.classList.remove("hovered", "delay-scale");
+      };
+
+      mapElement.addEventListener("mouseenter", () => {
+        if (!isHoveringAddress) {
+          isHoveringMap = true;
+          scaleUp();
+        }
+      });
+
+      mapElement.addEventListener("mouseleave", () => {
+        isHoveringMap = false;
+        if (!isHoveringAddress) scaleDown();
+      });
+
+      addressElement.addEventListener("mouseenter", () => {
+        if (!isHoveringMap) {
+          isHoveringAddress = true;
+          scaleUp();
+        }
+      });
+
+      addressElement.addEventListener("mouseleave", () => {
+        isHoveringAddress = false;
+        if (!isHoveringMap) scaleDown();
+      });
+    };
+
+    if (northMapRef.current && northAddressRef.current) {
+      handleHover(northMapRef.current, northAddressRef.current);
+    }
+
+    if (southMapRef.current && southAddressRef.current) {
+      handleHover(southMapRef.current, southAddressRef.current);
+    }
+  }, []);
+
   return (
     <div className="space-section">
       <div
@@ -18,6 +74,7 @@ const SpaceDesktop = () => {
           <div className={"space-top"}>
             <div className={"map"}>
               <a
+                ref={northMapRef}
                 className={"map-image-wrapper map-image-north"}
                 href="https://maps.app.goo.gl/V99mxENYgtX7sw3X9"
                 target="_blank"
@@ -25,7 +82,13 @@ const SpaceDesktop = () => {
               >
                 <img src={MapNorth} alt="Localização Norte" />
               </a>
-              <div className={"map-address address-north"}>
+              <div
+                onClick={() =>
+                  openLink("https://maps.app.goo.gl/V99mxENYgtX7sw3X9")
+                }
+                ref={northAddressRef}
+                className={"map-address address-north"}
+              >
                 <p className={"address-main-text"}>Asa Norte</p>
                 <div className={"address-details"}>
                   <div className={"pin-wrapper"}>
@@ -40,6 +103,7 @@ const SpaceDesktop = () => {
                 Atendimento Presencial
               </h3>
               <a
+                ref={southMapRef}
                 className={"map-image-wrapper map-image-south"}
                 href="https://maps.app.goo.gl/qDwMQZnpSivMpLRi7"
                 target="_blank"
@@ -47,7 +111,13 @@ const SpaceDesktop = () => {
               >
                 <img src={MapSouth} alt="Localização Sul" />
               </a>
-              <div className={"map-address address-south"}>
+              <div
+                ref={southAddressRef}
+                className={"map-address address-south"}
+                onClick={() =>
+                  openLink("https://maps.app.goo.gl/qDwMQZnpSivMpLRi7")
+                }
+              >
                 <p className={"address-main-text"}>Asa Sul</p>
                 <div className={"address-details"}>
                   <div className={"pin-wrapper"}>
